6c076893d954ff9c3522155b7cbae1736a94778b
ouijd <ouijdane.erraji@edu.uca.ma>
2021-07-20 23:34:35 +0100

    wijwija 1.0

---
+++ src/main/java/grptlkhra/com/michoshop/MichoshopApplication.java
@@ -0,0 +1,13 @@
+package grptlkhra.com.michoshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MichoshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MichoshopApplication.class, args);
+    }
+
+}
