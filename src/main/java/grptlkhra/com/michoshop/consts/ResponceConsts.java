0180d61025fffdb8608a01916c1dc6fffb287dd6
Mehdi <elwafi.courrier99@gmail.com>
2021-07-27 00:15:10 +0100

    Updated reponce

---
+++ src/main/java/grptlkhra/com/michoshop/consts/ResponceConsts.java
@@ -0,0 +1,13 @@
+package grptlkhra.com.michoshop.consts;
+
+public class ResponceConsts {
+    public static final String USERNAME_FOUND = "Username found!";
+    public static final String USERNAME_NOT_FOUND = "Username was not found!";
+    public static final String EMAIL_FOUND = "Email found!";
+    public static final String EMAIL_NOT_FOUND = "Email was not found!";
+    public static final String FORBIDDEN_MESSAGE = "You need to log to access this page";
+    public static final String ACCESS_DENIED_MESSAGE = "You do not have permission to acess this page";
+    public static final String SERVER_ERROR = "Error 500 Server error";
+    public static final String DATABASE_ERROR = "Conflicts in the database";
+    public static final String PAGE_NOT_FOUND = "Page not found error 404";
+}
--- src/main/java/grptlkhra/com/michoshop/consts/SecurityConsts.java
+++ src/main/java/grptlkhra/com/michoshop/consts/SecurityConsts.java
@@ -1,5 +1,17 @@
 package grptlkhra.com.michoshop.consts;
 
-public class SecurityConsts {
+import org.springframework.http.HttpStatus;
 
+public class SecurityConsts {
+    public static final long EXPIRATION_TIME = 432_000_000; // JWT 5 Days
+    public static final HttpStatus OK = HttpStatus.OK;
+    public static final String TOKEN_PREFIX = "Bearer ";
+    public static final String JWT_TOKEN_HEADER = "Jwt-Token";
+    public static final String TOKEN_CANNOT_BE_VERIFIED = "Token cannot be verified";
+    public static final String AUTHORITIES = "authorities";
+    public static final String OPTION_HTTP_METHOD = "OPTION";
+    public static final String[] PUBLIC_URLS = {"/user/all/**","/user/login/**","/user/register/**","/user/resetpassword/**","/user/image/**"};
+    public static final String COMPANY_LLC = "Company LLC";
+    public static final String COMPANY_LLC_ADMINISTRATON = "Company LLC administration";
+    public static final String timeZone = "Morocco/Casablanca";
 }
