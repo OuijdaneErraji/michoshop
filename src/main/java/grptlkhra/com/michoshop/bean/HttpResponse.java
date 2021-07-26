5a88dd1e9f0085fc66bf5d3b82274c7217218577
Mehdi <elwafi.courrier99@gmail.com>
2021-07-27 00:01:16 +0100

    Added const package

--- src/main/java/grptlkhra/com/michoshop/bean/HttpResponse.java
+++ src/main/java/grptlkhra/com/michoshop/bean/HttpResponse.java
@@ -1,5 +1,24 @@
 package grptlkhra.com.michoshop.bean;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import org.springframework.http.HttpStatus;
+
+import java.util.Date;
+
 public class HttpResponse {
-    
+    @JsonFormat(shape = JsonFormat.Shape.STRING,pattern = "MM-dd-yyyy hh:mm:ss",timezone = "Casablanca/Morocco")
+    private Date timeSamp;
+    private int httpStatusCode; // 200,100,400
+    private HttpStatus httpStatus;
+    private String reason; // Http Reason (Libelle d Http responce)
+    private String message; // Developper Msg
+
+
+    public HttpResponse(int httpStatusCode, HttpStatus httpStatus, String reason, String message) {
+        this.timeSamp = new Date();
+        this.httpStatusCode = httpStatusCode;
+        this.httpStatus = httpStatus;
+        this.reason = reason;
+        this.message = message;
+    }
 }
---
+++ src/main/java/grptlkhra/com/michoshop/consts/SecurityConsts.java
@@ -0,0 +1,5 @@
+package grptlkhra.com.michoshop.consts;
+
+public class SecurityConsts {
+
+}
